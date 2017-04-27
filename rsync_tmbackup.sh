74f0985f38d475491118eac1fedd041c79a72712
Sébastien Lesaint <sebastien.lesaint@gmail.com>
2017-04-27 23:47:15 +0200

    keep backup for 6 months rather than 31 days

--- rsync_tmbackup.sh
+++ rsync_tmbackup.sh
@@ -192,7 +192,7 @@ fi
 NOW=$(date +"%Y-%m-%d-%H%M%S")
 EPOCH=$(date "+%s")
 KEEP_ALL_DATE=$((EPOCH - 86400))       # 1 day ago
-KEEP_DAILIES_DATE=$((EPOCH - 2678400)) # 31 days ago
+KEEP_DAILIES_DATE=$((EPOCH - 15768000)) # 6 months
 
 export IFS=$'\n' # Better for handling spaces in filenames.
 DEST="$DEST_FOLDER/$NOW"
